c19e088d37870dca0292d2d56e8d4008cbde8b1f
19mohsin58 <mohsinjokhio320@gmail.com>
2026-07-17 22:13:40 +0200

    Ignore report/ folder and fix CoAP status anomaly calculation

--- cooja/sensor-node/resources/res-health-status.c
+++ cooja/sensor-node/resources/res-health-status.c
@@ -5,6 +5,7 @@
 /* Extern global variables from sensor-node.c */
 extern int heart_rate;
 extern int alert_active;
+extern int model_alert_active;
 
 static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 
@@ -19,10 +20,9 @@ RESOURCE(res_health_status,
 static void
 res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
-  int anomaly = (heart_rate > 100) ? 1 : 0;
   int length = snprintf((char *)buffer, preferred_size,
                         "{\"heart_rate\":%d,\"anomaly\":%d,\"alert\":%d}",
-                        heart_rate, anomaly, alert_active);
+                        heart_rate, model_alert_active, alert_active);
 
   coap_set_header_content_format(response, APPLICATION_JSON);
   coap_set_payload(response, buffer, length);
